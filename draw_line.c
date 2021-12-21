183c6d39cd2226bd41e9ed56e3a2a6944fcc844c
alfredpoly <36860113+Alfredpoly@users.noreply.github.com>
2021-12-21 14:05:55 +0100

    confinement

--- draw_line.c
+++ draw_line.c
@@ -6,7 +6,7 @@
 /*   By: fpolycar <fpolycar@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/17 14:21:37 by fpolycar      #+#    #+#                 */
-/*   Updated: 2021/12/17 15:22:15 by fpolycar      ########   odam.nl         */
+/*   Updated: 2021/12/17 15:29:04 by fpolycar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,13 +48,15 @@ void	init_line_var(t_arr_map	map, int x, int y, t_line *l)
 	l->y0 = map.map[y][x][1];
 	l->x1 = map.map[y][x + 1][0];
 	l->y1 = map.map[y][x + 1][1];
-	l->dx = abs(l->x1 - l->x0);
-	if (l->x0 < l->x1)
-		l->sx = -1;
-	l->dy = -abs(l->y1 - l->y0);
-	if (l->x0 > l->x1)
-		l->sy = 1;
-	l->err = l->dx + l->dy;
+	// l->dx = abs(l->x1 - l->x0);
+	// l->sx = l->x0 < l->x1 ? 1 : -1;
+	// // if (l->x0 < l->x1)
+	// // 	l->sx = -1;
+	// l->dy = -abs(l->y1 - l->y0);
+	// l->sy = l->y0 < l->y1 ? 1 : -1;
+	// // if (l->y0 < l->y1)
+	// // 	l->sy = -1;
+	// l->err = l->dx + l->dy;
 }
 
 // t_line	init_line_var_vert(t_arr_map	map, int x, int y, t_line l)
@@ -78,23 +80,24 @@ void	draw_line(t_arr_map	map, int x, int y, t_data img)
 	t_line	l;
 
 	init_line_var(map, x, y, &l);
-	while (1)
-	{
-		my_mlx_pixel_put(&img, l.x0, l.y0, 0x00FFFFFF);
-		if (l.x0 == l.x1 && l.y0 == l.y1)
-			break ;
-		l.e2 = 2 * l.err;
-		if (l.e2 >= l.dy)
-		{
-			l.err += l.dy;
-			l.x0 += l.sx;
-		}
-		if (l.e2 <= l.dx)
-		{
-			l.err += l.dx;
-			l.y0 += l.sy;
-		}
-	}
+	my_mlx_pixel_put(&img, l.x0, l.y0, 0x00FFFFFF);
+	// while (1)
+	// {
+		
+		// if (l.x0 == l.x1 && l.y0 == l.y1)
+		// 	break ;
+		// l.e2 = 2 * l.err;
+		// if (l.e2 >= l.dy)
+		// {
+		// 	l.err += l.dy;
+		// 	l.x0 += l.sx;
+		// }
+		// if (l.e2 <= l.dx)
+		// {
+		// 	l.err += l.dx;
+		// 	l.y0 += l.sy;
+		// }
+	// }
 }
 
 // void	draw_line_vert(t_arr_map map, int x, int y, t_data img)
